66cc583fcf9e710ad66bd48153e7037bc3ca5dce
kunshrestha <kunshrestha@hotmail.com>
2023-10-09 12:21:25 +1100

    Feign and eureka support

--- src/main/java/com/optimed/webapp/WebAppApplication.java
+++ src/main/java/com/optimed/webapp/WebAppApplication.java
@@ -2,8 +2,12 @@ package com.optimed.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class WebAppApplication {
 
 	public static void main(String[] args) {
