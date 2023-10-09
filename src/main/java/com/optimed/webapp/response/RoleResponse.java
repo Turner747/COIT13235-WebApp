bb552894026381d9d12172e7d0c46eff9887735c
kunshrestha <kunshrestha@hotmail.com>
2023-10-09 12:17:25 +1100

    Added @JsonIgnore

--- src/main/java/com/optimed/webapp/response/RoleResponse.java
+++ src/main/java/com/optimed/webapp/response/RoleResponse.java
@@ -1,5 +1,6 @@
 package com.optimed.webapp.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.util.Collection;
@@ -8,5 +9,6 @@ import java.util.Collection;
 public class RoleResponse {
     private long id;
     private String name;
+    @JsonIgnore
     private Collection<PrivilegeResponse> privileges;
 }
