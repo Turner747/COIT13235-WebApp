e488202bb96b544c6ea60d5e50c17e5ec58b34f3
kunshrestha <kunshrestha@hotmail.com>
2023-10-09 12:18:19 +1100

    DOB fix and postcode as string

--- src/main/java/com/optimed/webapp/response/StaffResponse.java
+++ src/main/java/com/optimed/webapp/response/StaffResponse.java
@@ -1,6 +1,7 @@
 package com.optimed.webapp.response;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Collection;
 import java.util.Date;
@@ -10,15 +11,16 @@ public class StaffResponse {
     private long id;
     private String firstName;
     private String lastName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
     private String street;
     private String suburb;
     private String state;
-    private int postcode;
+    private String postcode;
     private String phone;
     private String email;
     private String password;
     private String providerNumber;
     private String prescriberNumber;
-    private Collection<RoleResponse> roles;
+    private RoleResponse role;
 }
