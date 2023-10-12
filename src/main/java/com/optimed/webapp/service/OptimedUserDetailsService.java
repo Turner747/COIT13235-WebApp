fe9609e1fb8a534e3d79a72e8ca1543be218020a
kunshrestha <kunshrestha@hotmail.com>
2023-10-12 19:15:34 +1100

    Login page fixed with privileges

--- src/main/java/com/optimed/webapp/service/OptimedUserDetailsService.java
+++ src/main/java/com/optimed/webapp/service/OptimedUserDetailsService.java
@@ -1,13 +1,18 @@
 package com.optimed.webapp.service;
 
 import com.optimed.webapp.feignclient.StaffClient;
+import com.optimed.webapp.mappper.ObjectMapper;
+import com.optimed.webapp.response.PrivilegeResponse;
 import com.optimed.webapp.response.RoleResponse;
 import com.optimed.webapp.response.StaffResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -34,42 +39,39 @@ public class OptimedUserDetailsService implements UserDetailsService {
                     true,
                     true,
                     true,
-                    getAuthorities(Arrays.asList(staffClient.getRoleByName("ROLE_USER").getBody()))
+                    getAuthorities(staffClient.getRoleByName("ROLE_USER").getBody())
            );
         }
 
         return new org.springframework.security.core.userdetails.User(
                 staff.getEmail(), staff.getPassword(), true, true, true,
-                true, getAuthorities(Arrays.asList(staff.getRole())));
+                true, getAuthorities(staff.getRole()));
     }
 
     private Collection<? extends GrantedAuthority> getAuthorities(
-            Collection<RoleResponse> roles) {
+            RoleResponse role) {
 
-        return getGrantedAuthorities(getPrivileges(roles));
+        return getGrantedAuthorities(getPrivileges(role));
     }
 
-    private List<String> getPrivileges(Collection<RoleResponse> roles) {
-
-        /*List<String> privileges = new ArrayList<>();
-        List<Privilege> collection = new ArrayList<>();
-        for (Role role : roles) {
-            privileges.add(role.getName());
-            collection.addAll(role.getPrivileges());
-        }
-        for (Privilege item : collection) {
-            privileges.add(item.getName());
-        }
-        return privileges;*/
-        return new ArrayList<String>(); // placeholder
+    private List<String> getPrivileges(RoleResponse role) {
+        List<String> privileges = new ArrayList<>();
+        RoleResponse roleResponse = ObjectMapper.map(staffClient.getRoleByName(role.getName()).getBody(), RoleResponse.class);
+        System.out.println(roleResponse);
+        for(PrivilegeResponse p: roleResponse.getPrivileges())
+            privileges.add(p.getName());
+        return privileges;
     }
 
     private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
-        /*List<GrantedAuthority> authorities = new ArrayList<>();
+        List<GrantedAuthority> authorities = new ArrayList<>();
         for (String privilege : privileges) {
             authorities.add(new SimpleGrantedAuthority(privilege));
         }
-        return authorities;*/
-        return new ArrayList<GrantedAuthority>(); // placeholder
+        return authorities;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
